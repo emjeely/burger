4262184775cca7524a2b7c52b7c3537110aa6d7c
Emjeely <emilyumjee@gmail.com>
2019-02-26 21:22:29 -0700

    added top gif

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "emilyumjee331226",
+  password: "root",
   database: "burgers_db"
 });
 };
